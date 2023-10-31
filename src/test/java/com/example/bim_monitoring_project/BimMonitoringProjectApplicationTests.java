bb4ee80c0fa90d6a627bb57e0e7e25ad681a35ef
strangecodder@gmail.com <strangecodder@gmail.com>
2023-10-31 15:12:28 +0300

    Upload the project 31.10.2023

---
+++ src/test/java/com/example/bim_monitoring_project/BimMonitoringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bim_monitoring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimMonitoringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
